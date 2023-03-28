2e6c9c09742d5a2842158ff9b42cecd72570f0d1
Ethan Jones <ejones4224@gmail.com>
2023-03-28 14:29:23 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply); 
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4;
+console.log(mod); 
+
+let numbers = [5, 10, 20, 15, 7];
+let max = Math.max(...numbers);
+console.log(max);
